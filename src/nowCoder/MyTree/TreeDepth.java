e64d64004a9522445c71d361501a829d01e720a2
Dingji Zhang <zhangdingji110@163.com>
2025-11-06 00:30:44 +0800

    提交 二叉树深度

---
+++ src/nowCoder/MyTree/TreeDepth.java
@@ -0,0 +1,10 @@
+package nowCoder.MyTree;
+
+public class TreeDepth {
+    public int TreeDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(TreeDepth(node.left), TreeDepth(node.right)) + 1; // +1: 当前node对路径产生的影响
+    }
+}
---
+++ src/nowCoder/MyTree/TreeNode.java
@@ -0,0 +1,11 @@
+package nowCoder.MyTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
